5279c0578f497cf07ad19adf9a2a2f5d3d6ec9d1
Tamnguyen732 <thientam732@gmail.com>
2023-03-03 21:51:46 +1100

    post/ dalle api / created

---
+++ server/src/routes/postRoute.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+import Post from "../model/Post";
+
+dotenv.config();
+
+const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+router.route("/").get(async (req: Request, res: Response) => {
+  try {
+    const posts = await Post.find({});
+    console.log(posts);
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Fetching posts failed, please try again",
+    });
+  }
+});
+
+router.route("/").post(async (req: Request, res: Response) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to create a post, please try again",
+    });
+  }
+});
+
+export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,26 @@
-import express, { Application, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-
+import postRoutes from "./routes/postRoute";
+import dalleRoutes from "./routes/dalleRoutes";
 const port = 3000;
 dotenv.config();
 
 const app = express();
 app.use(cors());
+
 app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}.`);
-});
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`);
+  });
+};
+
+startServer();
